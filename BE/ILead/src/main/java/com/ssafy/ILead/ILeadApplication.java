a03558a7170de61ddb18fd594106511caf902e5a
곽현준 <kwak0568@naver.com>
2022-04-25 17:56:44 +0900

    feat: FE, BE 스켈레톤

---
+++ BE/ILead/src/main/java/com/ssafy/ILead/ILeadApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.ILead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ILeadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ILeadApplication.class, args);
+	}
+
+}
---
+++ BE/ILead/src/test/java/com/ssafy/ILead/ILeadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ILead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ILeadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
