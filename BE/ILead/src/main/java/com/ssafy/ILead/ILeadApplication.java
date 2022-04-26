7f1e65343a3c402daf6571371ebdf4ddc343c2f0
kwonohbeom <xpzmfhf@gmail.com>
2022-04-26 13:43:09 +0900

    feat<BE>: AWS S3 업로드 및 Cloud Front 설정

--- BE/ILead/src/main/java/com/ssafy/ILead/ILeadApplication.java
+++ BE/ILead/src/main/java/com/ssafy/ILead/ILeadApplication.java
@@ -1,13 +1,18 @@
 package com.ssafy.ILead;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class ILeadApplication {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location=" +
+			"classpath:application.yml, " +
+			"classpath:aws.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(ILeadApplication.class, args);
+		new SpringApplicationBuilder(ILeadApplication.class).properties(APPLICATION_LOCATIONS)
+				.run(args);
 	}
 
 }
